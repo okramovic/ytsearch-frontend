140d659104e8ddbab6e8522a25afd94b1260b747
Glitch (starter-react) <none>
2019-01-21 20:31:54 +0000

    👼👄 Checkpoint ./app/app.jsx:233481/63 ./app/components/App.jsx:233481/73 ./public/style.css:233481/43 ./app/index.html:233481/10

--- app/app.jsx
+++ app/app.jsx
@@ -1,7 +1,6 @@
 const React = require('react');
 const ReactDOM = require('react-dom');
 
-/* Import Components */
-const HelloWorld = require('./components/HelloWorld');
+const App = require('./components/App');
 
-ReactDOM.render(<HelloWorld/>, document.getElementById('main'));
+ReactDOM.render(<App/>, document.getElementById('main'));
--- app/components/App.jsx
+++ app/components/App.jsx
@@ -24,7 +24,7 @@ class HelloWorld extends React.Component {
     this.inputChangeHandler = this.inputChangeHandler.bind(this)
     
     this.state = {
-      input: 'array',
+      input: '',
       channels:[]
     }
   }
@@ -103,7 +103,7 @@ class HelloWorld extends React.Component {
       <form onSubmit={this.submitHandler}>
         <input id="main" type="text" placeholder="search" 
                onChange={this.inputChangeHandler} value={this.state.input} autocomplete="off" />
-        <p>fulltext search in your favorite YT channels</p>
+        <p>fulltext search in your</p>
       </form>
 
       <h1 id="searchTermHeader">header</h1>
@@ -117,7 +117,7 @@ class HelloWorld extends React.Component {
       
     </div>
   );
-  } 
+  } // fulltext search in your favorite YT channels
 }
 // <p>made with love in Vienna by me</p>
 
