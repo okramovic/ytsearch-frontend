8c1431d8d1689b6a2db46486bef4f1d574cfc040
Glitch (starter-react) <none>
2019-01-21 17:15:04 +0000

    🔪🎺 Checkpoint ./app/components/videoexpt.jsx:233481/2860 ./app/components/UnorderedList.jsx:233481/1363

--- app/components/UnorderedList.jsx
+++ app/components/UnorderedList.jsx
@@ -8,12 +8,26 @@ const UnorderedList = function({ channels }) {
   return (
     <ul>
       {channels.map(function(item, i) {
+        const CT = 'https://yt3.ggpht.com/a-/AAuE7mC56ctnjTBFVmFaDttL3sC26U2CRiICqBgJ-g=s288-mo-c-c0xffffffff-rj-k-no'
+        const Jeremy = 'https://yt3.ggpht.com/a-/AAuE7mBtvK6ioufwJFDGRT9WOvEykaDnAo0jGPOwvQ=s288-mo-c-c0xffffffff-rj-k-no'
+        const Siraj = 'https://yt3.ggpht.com/a-/AAuE7mAfNzuo-vOC8wdETbqIPU4UbnknsxCbCj3lLg=s176-c-k-c0x00ffffff-no-rj-mo'
+        const Twominute = 'https://yt3.ggpht.com/a-/AAuE7mBCBo51MwaRPnZWRH-BvZXhTCejRrbWhzYxuA=s48-mo-c-c0xffffffff-rj-k-no'
+        const channel = item.channel //this.props.channelname
+        let iconUrl = ''
+
+        if ( channel == 'Coding train') iconUrl = CT
+        else if (channel.match(/fastai/))  iconUrl = Jeremy
+        else if (channel.match(/siraj raval/i))  iconUrl = Siraj
+        else if (channel.match(/two minute papers/i))  iconUrl = Twominute
+        
+        
         console.log(item.channel, item.expt)
         return (
-          <div>
-            <h3>{item.channel}</h3>
-            <ChannelList key={i} expt={item.expt} channelname={item.channel}/>
-          </div>
+          <li classname="channel_outer">
+            <img src={channel} classname="channel_thumb"></img>
+            <h3 >{item.channel}</h3>
+            <ChannelList key={i} expt={item.expt} />
+          </li>
           
         )
         
--- app/components/videoexpt.jsx
+++ app/components/videoexpt.jsx
@@ -11,36 +11,25 @@ class VideoList extends React.Component{
     }
   }
   render(){
-    const CT = 'https://yt3.ggpht.com/a-/AAuE7mC56ctnjTBFVmFaDttL3sC26U2CRiICqBgJ-g=s288-mo-c-c0xffffffff-rj-k-no'
-    const Jeremy = 'https://yt3.ggpht.com/a-/AAuE7mBtvK6ioufwJFDGRT9WOvEykaDnAo0jGPOwvQ=s288-mo-c-c0xffffffff-rj-k-no'
-    const Siraj = 'https://yt3.ggpht.com/a-/AAuE7mAfNzuo-vOC8wdETbqIPU4UbnknsxCbCj3lLg=s176-c-k-c0x00ffffff-no-rj-mo'
-    const Twominute = 'https://yt3.ggpht.com/a-/AAuE7mBCBo51MwaRPnZWRH-BvZXhTCejRrbWhzYxuA=s48-mo-c-c0xffffffff-rj-k-no'
-    
-    const channel = this.props.channelname
-    let iconUrl = ''
-    
-    if ( channel == 'Coding train') iconUrl = CT
-    else if (channel.match(/fastai/))  iconUrl = Jeremy
-    else if (channel.match(/siraj raval/i))  iconUrl = Siraj
-    else if (channel.match(/two minute papers/i))  iconUrl = Twominute
+
       
       return (
         this.props.expt.map((x,i)=>{
           return (
-              <div key={i}>
+              <li key={i} classname="">
                 <div classname="channel_inner">
-                  <img src={channel} classname="channel_thumb"></img>
+                  
                   <h2 classname="channel_header">{x.title}</h2>
                 </div>
                
                 {x.excerpts.map((e,i)=>{
                   return (<div>
-                    <a href={x.id} target="_blank"></a>
-                    {e.text}
+                    <a href={x.id} target="_blank">{secondsToHumanTime(e.time)}</a>
+                    <p classname="excerpt_text">{e.text}</p>
                   </div>)
                  })
                 }
-              </div>
+              </li>
                            
           )
       })
@@ -51,3 +40,15 @@ class VideoList extends React.Component{
 }
 
 module.exports = VideoList;
+
+
+function secondsToHumanTime(num){
+    const sec = num % 60
+    const mins = ((num - sec)/60)%60
+    const hrs = Math.floor((num-sec)/3600)
+    
+    const secStr = sec<10? '0' + sec : sec,
+    minStr = mins<10? '0' + mins : mins,
+    hrsStr = hrs<10? '0' + hrs : hrs
+    return (hrsStr=='00'?'': hrsStr + ':') + minStr + ':' + secStr
+}
