303c12282de512fd32d3ac1bdef07f6279090c75
Glitch (starter-react) <none>
2019-01-26 14:17:16 +0000

    💄☃️ Checkpoint ./app/components/HeaderResults.jsx:233481/6 ./app/components/Info.js:233481/43 ./public/style.css:233481/77

--- app/components/HeaderResults.jsx
+++ app/components/HeaderResults.jsx
@@ -3,12 +3,12 @@ const React = require('react');
 module.exports = ({excCount, vidCount, prevInput})=>{
   
   if (!excCount) return(
-    <h1 id="searchTermHeader" className={ excCount===0? "":'hidden' }>Sorry, no results for "{prevInput}"</h1>
+    <h1 id="searchTermHeader" className={ excCount===0? "":'hidden' }>Sorry, no results for '{prevInput}'</h1>
   )
   
   return (
     <h1 id="searchTermHeader" className={excCount>0? "":'hidden' }>
-        Found {prevInput} in {excCount} excerpts in {vidCount} videos.
+        Found '{prevInput}' in {excCount} excerpts in {vidCount} videos.
     </h1>
   )
 }
