20261eef4d4e177893e929428555b250a281c55f
Glitch (starter-react) <none>
2019-01-26 10:29:05 +0000

    🌻📖 Checkpoint ./app/components/Info.js:233481/584

--- app/components/Info.js
+++ app/components/Info.js
@@ -10,7 +10,15 @@ class Info extends React.Component{
   }
   componentDidMount(){
     getEmptyVideos()
-    .then(res=>this.setState({channels: res}))
+    .then(res=>this.setState(prev=>{
+      
+      // store keys in state for toggling list visibility
+      res.map(channelInfo=>{
+        prev[channelInfo.channel] = false
+      })
+      
+      return {channels: res}
+    }))
   }
   render(){
     
@@ -30,11 +38,13 @@ class Info extends React.Component{
           return (
             <div>
               <h4 className="h_nocaps"> {chan.channel}</h4>
+              <div className={ this.state[chan.channel]? '': 'hidden'}>
               { chan.empty_videos.map((infoObj,i)=>{
                   const href = "https://youtu.be/" + infoObj.id
                   return <a key={i} href={href} target="_blank">{infoObj.title} {infoObj.ok_count}/{infoObj.ok_count + infoObj.empty_count}</a>
                 })
               }
+              </div>
             </div>
           )
          })
